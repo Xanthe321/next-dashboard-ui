f8734e0c01593955cb915050d6ebab8a7fd1e27e
Xanthe <104931289+Xanthe321@users.noreply.github.com>
2024-10-04 12:21:46 -0700

    test2

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,7 +1,14 @@
+import UserCard from "@/components/UserCard"
+
 const AdmingPage = () => {
     return(
-        <div className="">
-          AdminPage
+        <div className="p-4 flex gap-4 flex-col md:flex-row">
+          <div className="w-full lg:w-2/3">
+            <div className="">
+              <UserCard type=""/>
+            </div>
+          </div>
+          <div className="w-full lg:w-1/3">r</div>
         </div>
     )
 }
